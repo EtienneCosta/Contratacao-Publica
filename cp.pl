a60ac2757925920de6cca472a60c36d27bfd3596
Etienne Costa <etiennecosta@Air-de-Etienne.home>
2020-04-06 15:52:34 +0100

    Invariantes adicionados,evolucao e involucao do conhecimento +/-

--- cp.pl
+++ cp.pl
@@ -3,20 +3,22 @@
 :- set_prolog_flag( single_var_warnings,off ).
 :- set_prolog_flag(unknown,fail).
 
+
+:- op( 900, xfy,'::').
+:- dynamic (::)/2.
+:- dynamic '-'/1.
+:- dynamic (excecao/1).
+:- dynamic (adjudicante/4).
+:- dynamic (adjudicataria/4).
+:- dynamic (contrato/11).
+:- dynamic (anuncio/8).
+:- dynamic (concorrente/2).
+
+
 :-include('bc.pl').
 :-include('update.pl').
 :-include('invariantes.pl').
 :-include('auxiliar.pl').
-
-:- op( 900, xfy,'::').
-:- dynamic (::)/2.
-:- dynamic ('-'/1).
-:- dynamic (excecao/1).
-:- dynamic (adjudicante/4).
-:- dynamic (adjudicataria/4).
-:- dynamic (contrato/10).
-:- dynamic (anuncio/9).
-:- dynamic (concorrente/2).
 
 
 
@@ -29,8 +31,22 @@
 									nao(excecao(adjudicante(IdAd,Nome,NIF,Morada))).
 
 -adjudicataria(IdAda,Nome,NIF,Morada):- 
-									nao(adjudicataria(IdAd,Nome,NIF,Morada)),
-									nao(excecao(adjudicataria(IdAd,Nome,NIF,Morada))).
+									nao(adjudicataria(IdAda,Nome,NIF,Morada)),
+									nao(excecao(adjudicataria(IdAda,Nome,NIF,Morada))).
+
+-contrato(IdC,IdAd,IdAda,IdsAnuncio,TipoContrato,TipoProcedimento,Descricao ,Valor,Prazo,Local,Data):-
+									nao(contrato(IdC,IdAd,IdAda,IdsAnuncio,TipoContrato,TipoProcedimento,Descricao ,Valor,Prazo,Local,Data)),
+									nao(excecao(contrato(IdC,IdAd,IdAda,IdsAnuncio,TipoContrato,TipoProcedimento,Descricao ,Valor,Prazo,Local,Data))).		
+
+-anuncio(IdAnuncio,IdAd,Nome,Descricao,TipoContrato,Preco,Prazo,Data):-
+									nao(anuncio(IdAnuncio,IdAd,Nome,Descricao,TipoContrato,Preco,Prazo,Data)),
+									nao(excecao(anuncio(IdAnuncio,IdAd,Nome,Descricao,TipoContrato,Preco,Prazo,Data))).
+
+-concorrente(IdAnuncio,IdsAd):-		
+									nao(concorrente(IdAnuncio,IdsAd)),
+									nao(excecao(concorrente(IdAnuncio,IdsAd))).
+
+
 
 
 
--- invariantes.pl
+++ invariantes.pl
@@ -2,3 +2,184 @@
 %------------------------------------------------------------------------------%
 %-------------------------------- Invariantes ---------------------------------%
 %------------------------------------------------------------------------------%
+
+
+
+
+%-------------------------------- Invariantes Estruturais e Referenciais: Adjudicante  ---------------------------------%
+% adjudicante: #IdAd, Nome, NIF , Morada 
+
+% Invariante que garante que o id de cada Adjudicante é único para conhecimento positivo.
+
++adjudicante(IdAd,_,_,_)::(integer(IdAd),
+						   findall(IdAd,adjudicante(IdAd,_,_,_),R),
+						   length(R,1)).
+
+% Invariante que garante que o NIF de cada Adjudicante é único para conhecimento positivo.
+
++adjudicante(_,_,NIF,_)::(findall(NIF,adjudicante(_,_,NIF,_),R),
+									length(R,1)).
+
+% Invariante que garante que adjudicantes com ids  diferentes têm diferente informação .
+
++adjudicante(IdAd,Nome,NIF,Morada)::((findall((Nome,NIF,Morada),adjudicante(_,Nome,NIF,Morada),R),
+									 length(R,1))).
+
+
+% Invariante que garante que o id de cada Adjudicante é único para conhecimento negativo.
+
+
++(-adjudicante(IdAd,_,_,_))::(integer(IdAd),
+						   findall(IdAd,-adjudicante(IdAd,_,_,_),R),
+						   length(R,1)).
+
+% Invariante que garante que o NIF de cada Adjudicante é único para conhecimento negativo.
+
++(-adjudicante(_,_,NIF,_))::(findall(NIF,-adjudicante(_,_,NIF,_),R),
+									length(R,1)).
+
+
+% Invariante que garante que adjudicantes com ids  diferentes têm diferente informação.
+
++(-adjudicante(IdAd,Nome,NIF,Morada))::((findall((Nome,NIF,Morada),-adjudicante(_,Nome,NIF,Morada),R),
+									 length(R,1))).
+
+-adjudicante(IdAd,_,_,_)::(findall(IdAd,adjudicante(IdAd,_,_,_),R),
+						   length(R,0)).
+
+-adjudicante(IdAd,_,_,_)::(findall(IdAd,contrato(_,IdAd,_,_,_,_,_,_,_,_,_),R),
+						   length(R,0)).
+
+
+
+%-------------------------------- Invariantes Estruturais e Referenciais: Adjudicatarias  ---------------------------------%
+
+% adjudicatária: #IdAda, Nome, NIF , Morada ↝ { 𝕍,𝔽,𝔻 }
+
+% Invariante que garante que o id de cada adjudicataria é único para conhecimento positivo.
+
++adjudicataria(IdAda,_,_,_)::(integer(IdAda),
+						   	  findall(IdAda,adjudicataria(IdAda,_,_,_),R),
+						      length(R,1)).
+
+
+% Invariante que garante que o NIF de cada Adjudicante é único para conhecimento positivo.
+
++adjudicataria(_,_,NIF,_)::(findall(NIF,adjudicataria(_,_,NIF,_),R),
+									length(R,1)).
+
+% Invariante que garante que adjudicantes com ids  diferentes têm diferente informação .
+
++adjudicataria(IdAda,Nome,NIF,Morada)::((findall((Nome,NIF,Morada),adjudicataria(_,Nome,NIF,Morada),R),
+									     length(R,1))).
+
+
+% Invariante que garante que o id de cada Adjudicante é único para conhecimento negativo.
+
+
++(-adjudicataria(IdAda,_,_,_))::(integer(IdAda),
+						         findall(IdAda,-adjudicataria(IdAda,_,_,_),R),
+						         length(R,1)).
+
+% Invariante que garante que o NIF de cada Adjudicante é único para conhecimento negativo.
+
++(-adjudicataria(_,_,NIF,_))::(findall(NIF,-adjudicataria(_,_,NIF,_),R),
+									length(R,1)).
+
+
+% Invariante que garante que adjudicantes com ids  diferentes têm diferente informação.
+
++(-adjudicataria(IdAda,Nome,NIF,Morada))::((findall((Nome,NIF,Morada),-adjudicataria(_,Nome,NIF,Morada),R),
+									 length(R,1))).
+
+
+-adjudicataria(IdAda,_,_,_)::(findall(IdAda,adjudicataria(IdAda,_,_,_),R),
+						    length(R,0)).
+
+-adjudicataria(IdAda,_,_,_)::(findall(IdAda,contrato(_,_,IdAda,_,_,_,_,_,_,_,_),R),
+						   length(R,0)).
+
+%-------------------------------- Invariantes Estruturais e Referenciais: Contrato  ---------------------------------%
+
+
+% contrato: #IdC ,#IdAd, #IdAda,[#IdAnuncio], Tipo de Contrato, Tipo de Procedimento , Descrição , Valor , Prazo , Local ,Data ↝ { 𝕍,𝔽,𝔻 }
+
+% contratos válidos :(['Aquisicao de servicos','Locacao de bens moveis','Contrato de aquisicao']).
+% procedimentos válidos : (['Ajuste direto','Consulta previa','Concurso publico']).
+
++contrato(Idc,_,_,_,_,_,_,_,_,_,_)::(integer(Idc),
+									 findall(Idc,contrato(Idc,_,_,_,_,_,_,_,_,_,_),R),
+								     length(R,1)).
+
+
++contrato(_,IdAd,IdAda,_,_,_,_,_,_,_,_)::(integer(IdAd),
+										  integer(IdAda),
+										  findall(IdAd,adjudicante(IdAd,_,_,_),R1),
+										  findall(IdAda,adjudicataria(IdAda,_,_,_),R2),
+								    	  length(R1,1),
+								    	  length(R2,1)).
+
++contrato(_,_,_,_,_,_,_,_,_,_,Data)::(validaData(Data)).
+
+
++contrato(_,_,_,IdsAnuncio,TipoContrato,TipoProcedimento,_,Valor,Prazo,_,Data)::procedimento(_,_,_,IdsAnuncio,TipoContrato,TipoProcedimento,_,Valor,Prazo,_,Data).
+
+
+
++(-contrato(Idc,_,_,_,_,_,_,_,_,_,_))::(integer(Idc),
+									 findall(Idc,-contrato(Idc,_,_,_,_,_,_,_,_,_,_),R),
+								     length(R,1)).
+																
+
++(-contrato(_,IdAd,IdAda,_,_,_,_,_,_,_,_))::(integer(IdAd),
+										     integer(IdAda),
+										  	 findall(IdAd,adjudicante(IdAd,_,_,_),R1),
+										     findall(IdAda,adjudicataria(IdAda,_,_,_),R2),
+								    	     length(R1,1),
+								    	     length(R2,1)).														
+
+
++(-contrato(_,_,_,_,_,_,_,_,_,_,Data))::(validaData(Data)).
+
+
++(-contrato(_,_,_,IdsAnuncio,TipoContrato,TipoProcedimento,_,Valor,Prazo,_,Data))::procedimento(_,_,_,IdsAnuncio,TipoContrato,TipoProcedimento,_,Valor,Prazo,_,Data).
+
+
+%Remocao de contrato 
+
+%-------------------------------- Invariantes Estruturais e Referenciais: Anúncio  ---------------------------------%
+
+% anúncio: #IdAnuncio, #IdAd, Nome,Descrição,Tipo de Contrato,Preço, Prazo, Data.
+
+
++anuncio(IdAnuncio,IdAd,Nome,_,_,_,_,_)::(integer(IdAnuncio),
+												 integer(IdAd),
+												 findall((IdAd,Nome),adjudicante(IdAd,Nome,_,_),R1),
+												 length(R1,1),
+												 findall(IdAnuncio,anuncio(IdAnuncio,_,_,_,_,_,_,_),R2),
+												 length(R2,1)).
+
++anuncio(_,_,_,_,TipoContrato,Preco,Prazo,Data)::(Preco>0,
+									   Prazo>0,
+								       validaData(Data),
+                                       pertence(TipoContrato,['Aquisicao de servicos','Locacao de bens moveis','Contrato de aquisicao'])).
+
+
+
+%Remocao de anuncio
+
+
+
+%-------------------------------- Invariantes Estruturais e Referenciais: Concorrente  ---------------------------------%
+
+% concorrente: #IdAnuncio, [#IdAd].
+
++concorrente(IdAnuncio,IdsAd)::(findall(IdAnuncio,concorrente(IdAnuncio,_),R),
+								length(R,1),
+								foreach(IdsAd),
+								findall(Id,adjudicante(Id,_,_,_),L),
+								pertence2(IdsAd,L)).
+
+
+
+
